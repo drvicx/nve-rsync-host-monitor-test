4a67ba07f1e008fd59e9e5332cf49a095c71a6d1
VictorNuzhdin <nuzhdin.vic@gmail.com>
2026-07-01 15:12:13 +0600

    GitHub Workflow: Testing update mode (5)

--- app/routers/data.py
+++ app/routers/data.py
@@ -63,5 +63,5 @@ async def get_data(db: Session = Depends(get_db)):
             "osVersion": os_version,
             "osKernel": os_kernel
         },
-        "debugMessage": "test_202607_4"
+        "debugMessage": "test_202607_5"
     }
